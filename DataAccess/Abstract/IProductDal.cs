0cfed47955e1bb61bfdb448375bc2e423204e9cf
Faruk Yakut <84045640+farukyakut@users.noreply.github.com>
2023-07-11 11:27:39 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+
+namespace DataAccess.Abstract;
+
+public interface IProductDal
+{
+    List<Product> GetAll();
+    void Add(Product product);
+    void Update(Product product);
+    void Delete(Product product);
+    List<Product> GetAllCategory(int categoryId);
+}

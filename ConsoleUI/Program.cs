81279460bedbf5c5f5d1e71e1e8a59941d2f0f8b
Faruk Yakut <84045640+farukyakut@users.noreply.github.com>
2023-07-13 08:49:08 +0300

    Gün 8

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 Console.WriteLine("Hello, World!");
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+//Solid
+//O(Open Closed Principle) == yaptığın yazılıma yeni bir özellik ekliyorsa mevcuttaki hiçbir koda dokunmamlısın
 
-foreach (var product in productManager.GetAll())
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+//foreach (var product in productManager.GetAll())
+//{
+//    Console.WriteLine(product.ProductName);
+//}
+
+foreach (var product in productManager.GetByUnitPrice(50,100))
+{
+    Console.WriteLine(product.ProductName);
+}
+
+foreach (var product in productManager.GetAllByCategoryId(5))
 {
     Console.WriteLine(product.ProductName);
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans Tip
+    //IEntity : IEntity olabilir veya IEntity implement eden bir sınıf olabilir.
+    // new() : new'lenebilir olmalı (class), interface new'lenemez
+    public interface IEntityRepository<T> where T : class,IEntity, new()
+    {
+        //Generic Repository
+
+        //Filtreleme yapabilmek için " Expression<Func<T,bool>> filter=null " ifadesi kullanılır.
+        //filter = null , filtre vermek zorunda değiliz demektir.
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        // Detay işlemlerinde kullanılcaktır
+        // filtre kullanılmak zorundadır. Çünkü filter = null değil.
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}

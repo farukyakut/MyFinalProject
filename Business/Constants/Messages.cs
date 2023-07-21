99f69cd48cf96e9f3b3a13c73d983f329dd0d422
Faruk Yakut <84045640+farukyakut@users.noreply.github.com>
2023-07-21 12:37:44 +0300

    Gün 11 Ders Videosu eklentileri tamamlandı

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,7 +12,7 @@ namespace Business.Constants
     {
         public static string ProductAded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        public static string MaintenanceTime = "Sistem bakında";
+        public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler Listelendi";
     }
 }
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loseely Coupled 
+        //namin Convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                //result.Data'a yapılırsa sadece datayı alabilirsin
+                return Ok(result);
+            }
+            
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]  
+        public IActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product) 
+        {
+            var result = _productService.Add(product);
+            if (result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}

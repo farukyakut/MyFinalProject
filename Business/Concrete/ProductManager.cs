50774ccec49ac9e58754575a06857510aafd3bd0
Faruk Yakut <84045640+farukyakut@users.noreply.github.com>
2023-07-23 22:33:38 +0300

    Gün 12 Autofac, Fluentvalidation ve aspect eklendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System.Collections.Generic;
 using System.Data;
 
@@ -17,13 +21,14 @@ public class ProductManager : IProductService
         _productDal = productDal;
     }
 
+    [ValidationAspect(typeof(ProductValidator))]
     public IResult Add(Product product)
     {
-        if(product.ProductName.Length < 2)
-        {
-            //magic string
-            return new ErrorResult(Messages.ProductNameInvalid);
-        }
+        
+     
+
+        //business codes
+
         _productDal.Add(product);
         return new SuccessResult(Messages.ProductAded);
     }
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -36,7 +36,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet("getbyid")]  
-        public IActionResult Get(int id)
+        public IActionResult GetById(int id)
         {
             var result = _productService.GetById(id);
             if (result.Success)
